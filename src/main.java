ffde1763308adc45a7f68f54aa1e56f545c17de5
Sergey Miller <sergmiller1976@gmail.com>
2022-08-20 21:01:48 +0300

    add Java2

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        double ticket = 80.0;
+        double amount = 20.0;
+        int persentTicket = 1;
+        double s = ticket / amount * persentTicket;
+
+        System.out.println(s);
+    }
+
+}
---
+++ src/main1.java
@@ -0,0 +1,20 @@
+public class main1 {
+    public static void main(String[] args) {
+        int cost = 100;
+        int total1 = cost + 100;
+        int i1 = 1000;
+        int s = 0;
+        if (s >= i1) {
+
+        } else {
+            int bonus = 11;
+            s = bonus + total1 + i1;
+        }
+
+        System.out.println(total1);
+        System.out.println(s);
+
+    }
+
+}
+
